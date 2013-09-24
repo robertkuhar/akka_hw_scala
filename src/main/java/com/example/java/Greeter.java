50cf7ebfa4b53364cbd4e2e834afe6c76419fb96
Bob Kuhar <Robert_Kuhar@htc.com>
2013-09-24 15:31:19 -0700

    Java implementation complete

---
+++ src/main/java/com/example/java/Greeter.java
@@ -0,0 +1,20 @@
+package com.example.java;
+
+import akka.actor.*;
+
+public class Greeter extends UntypedActor {
+    public static enum Msg {
+        GREET, DONE;
+    }
+
+    @Override
+    public void onReceive( Object msg ) throws Exception {
+        if ( msg == Msg.GREET ) {
+            System.out.println( String.format( "Hello World from %s", getClass().getName() ) );
+            getSender().tell( Msg.DONE, getSelf() );
+        } else {
+            unhandled( msg );
+        }
+    }
+
+}
